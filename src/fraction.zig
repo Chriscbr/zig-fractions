f9e92db3f7c9f9a055f0cc09306035ec9ff23c42
Christopher Rybicki <crybicki98@gmail.com>
2024-07-06 01:37:43 -0400

    refactor memory representation to use usize for num and denom, and sign bit

--- src/fraction.zig
+++ src/fraction.zig
@@ -5,11 +5,6 @@ pub const FractionError = error{
     DenominatorCannotBeZero,
 };
 
-// TODO: refactor implementation to store the numerator and
-// denominator as unsigned values, and add a sign field.
-// TODO: or better, allow numerator and denominator to be
-// stored as any unsigned integer type
-
 // TODO: toString()
 // TODO: format()
 // TODO: to(comptime type T) T
@@ -24,44 +19,38 @@ pub const FractionError = error{
 
 pub const Fraction = struct {
     /// The numerator of the fraction.
-    num: i32,
+    num: usize,
 
-    /// The denominator of the fraction. Must not be zero.
-    denom: i32,
+    /// The denominator of the fraction.
+    denom: usize,
 
-    /// Initialize the fraction with the given numerator and denominator.
+    /// The sign bit of the fraction. True if the fraction is negative.
+    sign: bool,
+
+    /// Initialize the fraction. The sign bit is true if the fraction is negative.
     /// The denominator must not be zero.
-    pub fn init(num: i32, denom: i32) !Fraction {
+    pub fn init(num: usize, denom: usize, sign: bool) !Fraction {
         if (denom == 0) {
             return FractionError.DenominatorCannotBeZero;
         }
-        return Fraction{ .num = num, .denom = denom };
+        return Fraction{ .num = num, .denom = denom, .sign = sign };
     }
 
     /// Modify to become the absolute value.
-    pub fn abs(self: *Fraction) !void {
-        if (self.num < 0) {
-            self.num = try math.sub(i32, 0, self.num);
-        }
-        if (self.denom < 0) {
-            self.denom = try math.sub(i32, 0, self.denom);
+    pub fn abs(self: *Fraction) void {
+        if (self.sign == true) {
+            self.sign = false;
         }
     }
 
     /// Return a new fraction that is the absolute value.
     pub fn toAbs(self: Fraction) Fraction {
-        const num = if (self.num < 0) -self.num else self.num;
-        const denom = if (self.denom < 0) -self.denom else self.denom;
-        return Fraction{ .num = num, .denom = denom };
+        return Fraction{ .num = self.num, .denom = self.denom, .sign = false };
     }
 
     /// Modify to become the negative.
-    pub fn negate(self: *Fraction) !void {
-        if (self.denom > 0) {
-            self.num = try math.sub(i32, 0, self.num);
-        } else {
-            self.denom = try math.sub(i32, 0, self.denom);
-        }
+    pub fn negate(self: *Fraction) void {
+        self.sign = !self.sign;
     }
 
     /// Modify to become the reciprocal.
@@ -76,7 +65,7 @@ pub const Fraction = struct {
 
     /// Return a new fraction that is the reciprocal.
     pub fn toReciprocal(self: Fraction) !Fraction {
-        const result = Fraction{ .num = self.denom, .denom = self.num };
+        const result = Fraction{ .num = self.denom, .denom = self.num, .sign = self.sign };
         if (result.denom == 0) {
             return FractionError.DenominatorCannotBeZero;
         }
@@ -84,28 +73,24 @@ pub const Fraction = struct {
     }
 
     /// Returns true if the two fractions are equal.
-    pub fn eql(a: *const Fraction, b: *const Fraction) bool {
-        return a.num * b.denom == b.num * a.denom;
+    pub fn eql(a: *const Fraction, b: *const Fraction) !bool {
+        const r1 = try math.mul(usize, a.num, b.denom);
+        const r2 = try math.mul(usize, b.num, a.denom);
+        return a.sign == b.sign and r1 == r2;
     }
 
     /// Returns true if the two fractions are equal in absolute value.
     pub fn eqlAbs(a: *const Fraction, b: *const Fraction) !bool {
-        const r1 = try math.mul(usize, @abs(a.num), @abs(b.denom));
-        const r2 = try math.mul(usize, @abs(b.num), @abs(a.denom));
+        const r1 = try math.mul(usize, a.num, b.denom);
+        const r2 = try math.mul(usize, b.num, a.denom);
         return r1 == r2;
     }
 
     /// Simplify the fraction.
     pub fn simplify(self: *Fraction) void {
-        const gcd_result: usize = std.math.gcd(@abs(self.num), @abs(self.denom));
-        // Safe cast because GCD of two i32s is always <= 2147483647
-        const gcd: i32 = @intCast(gcd_result);
+        const gcd = std.math.gcd(self.num, self.denom);
         self.num = @divExact(self.num, gcd);
         self.denom = @divExact(self.denom, gcd);
-        if (self.denom < 0) {
-            self.num = -self.num;
-            self.denom = -self.denom;
-        }
     }
 };
 
--- src/fraction_test.zig
+++ src/fraction_test.zig
@@ -9,114 +9,115 @@ const Fraction = @import("Fraction.zig").Fraction;
 const FractionError = @import("Fraction.zig").FractionError;
 
 test "init" {
-    const f = try Fraction.init(1, 2);
+    const f = try Fraction.init(1, 2, false);
     try expectEqual(1, f.num);
     try expectEqual(2, f.denom);
+    try expectEqual(false, f.sign);
 
-    const err = Fraction.init(1, 0);
+    const err = Fraction.init(1, 0, false);
     try expectError(FractionError.DenominatorCannotBeZero, err);
 }
 
 test "abs" {
-    var f1 = try Fraction.init(-1, 2);
-    try f1.abs();
+    var f1 = try Fraction.init(1, 2, false);
+    f1.abs();
     try expectEqual(1, f1.num);
     try expectEqual(2, f1.denom);
+    try expectEqual(false, f1.sign);
 
-    var f2 = try Fraction.init(1, -2);
-    try f2.abs();
+    var f2 = try Fraction.init(1, 2, true);
+    f2.abs();
     try expectEqual(1, f2.num);
     try expectEqual(2, f2.denom);
-
-    var f3 = try Fraction.init(-1, -2);
-    try f3.abs();
-    try expectEqual(1, f3.num);
-    try expectEqual(2, f3.denom);
-
-    var f4 = try Fraction.init(std.math.minInt(i32), 1);
-    const err = f4.abs();
-    try expectError(error.Overflow, err);
+    try expectEqual(false, f2.sign);
 }
 
 test "toAbs" {
-    const f1 = try Fraction.init(-1, 2);
+    const f1 = try Fraction.init(1, 2, false);
     const r1 = f1.toAbs();
     try expectEqual(1, r1.num);
     try expectEqual(2, r1.denom);
+    try expectEqual(false, r1.sign);
 
-    const f2 = try Fraction.init(1, -2);
+    const f2 = try Fraction.init(1, 2, true);
     const r2 = f2.toAbs();
     try expectEqual(1, r2.num);
     try expectEqual(2, r2.denom);
-
-    const f3 = try Fraction.init(-1, -2);
-    const r3 = f3.toAbs();
-    try expectEqual(1, r3.num);
-    try expectEqual(2, r3.denom);
+    try expectEqual(false, r2.sign);
 }
 
 test "negate" {
-    var f1 = try Fraction.init(1, 2);
-    try f1.negate();
-    try expectEqual(-1, f1.num);
+    var f1 = try Fraction.init(1, 2, false);
+    f1.negate();
+    try expectEqual(1, f1.num);
     try expectEqual(2, f1.denom);
+    try expectEqual(true, f1.sign);
 
-    var f2 = try Fraction.init(-1, 2);
-    try f2.negate();
+    var f2 = try Fraction.init(1, 2, true);
+    f2.negate();
     try expectEqual(1, f2.num);
     try expectEqual(2, f2.denom);
-
-    var f3 = try Fraction.init(std.math.minInt(i32), 1);
-    const err = f3.negate();
-    try expectError(error.Overflow, err);
+    try expectEqual(false, f2.sign);
 }
 
 test "reciprocal" {
-    var f1 = try Fraction.init(1, 2);
+    var f1 = try Fraction.init(1, 2, false);
     try f1.reciprocal();
     try expectEqual(2, f1.num);
     try expectEqual(1, f1.denom);
+    try expectEqual(false, f1.sign);
 
-    var f2 = try Fraction.init(0, 1);
+    var f2 = try Fraction.init(0, 1, false);
     const err = f2.reciprocal();
     try expectError(FractionError.DenominatorCannotBeZero, err);
 }
 
 test "toReciprocal" {
-    const f1 = try Fraction.init(1, 2);
+    const f1 = try Fraction.init(1, 2, false);
     const r = try f1.toReciprocal();
     try expectEqual(2, r.num);
     try expectEqual(1, r.denom);
+    try expectEqual(false, r.sign);
 
-    const f2 = try Fraction.init(0, 1);
+    const f2 = try Fraction.init(0, 1, false);
     const err = f2.toReciprocal();
     try expectError(FractionError.DenominatorCannotBeZero, err);
 }
 
 test "eql" {
-    const f1 = try Fraction.init(1, 2);
-    const f2 = try Fraction.init(-2, -4);
-    try expect(Fraction.eql(&f1, &f2));
+    const f1 = try Fraction.init(1, 2, false);
+    const f2 = try Fraction.init(3, 6, false);
+    try expect(try Fraction.eql(&f1, &f2));
+
+    const f3 = try Fraction.init(3, 6, true);
+    try expect(!try Fraction.eql(&f1, &f3));
+
+    const f4 = try Fraction.init(std.math.maxInt(usize), std.math.maxInt(usize), false);
+    const f5 = try Fraction.init(std.math.maxInt(usize) - 1, std.math.maxInt(usize) - 1, false);
+    const err = Fraction.eql(&f4, &f5);
+    try expectError(error.Overflow, err);
 }
 
 test "eqlAbs" {
-    const f1 = try Fraction.init(1, 2);
-    const f2 = try Fraction.init(-2, 4);
+    const f1 = try Fraction.init(1, 2, false);
+    const f2 = try Fraction.init(2, 4, false);
     try expect(try Fraction.eqlAbs(&f1, &f2));
 
-    const f3 = try Fraction.init(std.math.maxInt(i32), std.math.minInt(i32));
-    const f4 = try Fraction.init(std.math.minInt(i32), std.math.maxInt(i32));
-    try expect(!try Fraction.eqlAbs(&f3, &f4));
+    const f3 = try Fraction.init(4, 6, false);
+    const f4 = try Fraction.init(2, 3, true);
+    try expect(try Fraction.eqlAbs(&f3, &f4));
 }
 
 test "simplify" {
-    var f1 = try Fraction.init(-4, -6);
+    var f1 = try Fraction.init(4, 6, false);
     f1.simplify();
     try expectEqual(2, f1.num);
     try expectEqual(3, f1.denom);
+    try expectEqual(false, f1.sign);
 
-    var f2 = try Fraction.init(4, -6);
+    var f2 = try Fraction.init(4, 6, true);
     f2.simplify();
-    try expectEqual(-2, f2.num);
+    try expectEqual(2, f2.num);
     try expectEqual(3, f2.denom);
+    try expectEqual(true, f2.sign);
 }
